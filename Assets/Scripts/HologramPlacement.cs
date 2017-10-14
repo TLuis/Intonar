bf248f5bcda545df99bdd1230ce49506ff634ab5
Lars <lars@lars-rasmussen.de>
2017-10-14 12:14:07 +0200

    Add speech recognition to activate the free moving mode of the hologram.

--- Assets/Scripts/HologramPlacement.cs
+++ Assets/Scripts/HologramPlacement.cs
@@ -5,16 +5,24 @@ public class HologramPlacement : MonoBehaviour
     public enum Mode
     {
         FIXED, 
-        MOVING
+        MOVING,
+        ROTATING
+    };
+
+    public enum Axis
+    {
+        ALL, X, Y, Z
     };
 
     public Mode currentMode = Mode.FIXED;
+    public Axis axis = Axis.ALL;
     public Vector3 initialPosHolgram;
     public Quaternion initialRotationHologram; 
     public Vector3 initialPosCamera;
     public Quaternion initialRotationCamera;
-    public GameObject cameraObj; 
+    public GameObject cameraObj;
 
+    /*
     // Called by GazeGestureManager when the user performs a Select gesture
     void OnSelect()
     {
@@ -34,21 +42,42 @@ public class HologramPlacement : MonoBehaviour
             Debug.LogError("Unknown placement mode!");
         }
     }
+    */
+
+    void OnFix()
+    {
+        currentMode = Mode.FIXED;
+    }
+
+    void OnMoving(Axis axis)
+    {
+        this.axis = axis;
+        currentMode = Mode.MOVING;
+        initialPosHolgram = this.transform.localPosition;
+        initialPosCamera = cameraObj.transform.localPosition;
+    }
+
+    void OnRotating()
+    {
+        currentMode = Mode.ROTATING;
+    }
 
     private void Update()
     {
         if (currentMode == Mode.FIXED)
         {
 
-        } else if(currentMode == Mode.MOVING)
+        } else if (currentMode == Mode.MOVING)
         {
             this.transform.localPosition = initialPosHolgram + (cameraObj.transform.localPosition - initialPosCamera);
+        } else if (currentMode == Mode.ROTATING)
+        {
             float cameraRotation = cameraObj.transform.localRotation.eulerAngles.y;
             float initialCameraRotation = initialRotationCamera.eulerAngles.y;
             float rotateHologramAngle = cameraRotation - initialCameraRotation;
             Quaternion rotation = Quaternion.Euler(0.0f, rotateHologramAngle, 0.0f);
             this.transform.localRotation = rotation * initialRotationHologram;
-        } else
+        } else 
         {
             Debug.LogError("Unknown placement mode!");
         }
---
+++ Assets/Scripts/SpeechManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class SpeechManager : MonoBehaviour
+{
+    KeywordRecognizer keywordRecognizer = null;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    // Use this for initialization
+    void Start()
+    {
+        keywords.Add("Fix", () =>
+        {
+            this.BroadcastMessage("OnFix");
+        });
+
+        keywords.Add("Move", () =>
+        {
+            this.BroadcastMessage("OnMoving", HologramPlacement.Axis.ALL);
+        });
+
+        keywords.Add("Move x", () =>
+        {
+            this.BroadcastMessage("OnMoving", HologramPlacement.Axis.X);
+        });
+
+        keywords.Add("Move y", () =>
+        {
+            this.BroadcastMessage("OnMoving", HologramPlacement.Axis.Y);
+        });
+
+        keywords.Add("Move z", () =>
+        {
+            this.BroadcastMessage("OnMoving", HologramPlacement.Axis.Z);
+        });
+
+        keywords.Add("Rotate", () =>
+        {
+            this.BroadcastMessage("OnRotating");
+        });
+
+        keywords.Add("Play", () =>
+        {
+            this.BroadcastMessage("OnMoving", HologramPlacement.Axis.Z);
+        });
+
+        keywords.Add("Reset", () =>
+        {
+            this.BroadcastMessage("OnMoving", HologramPlacement.Axis.Z);
+        });
+
+        // Tell the KeywordRecognizer about our keywords.
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+
+        // Register a callback for the KeywordRecognizer and start recognizing!
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+    }
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
+}
